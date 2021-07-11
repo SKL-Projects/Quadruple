23094ea4400c8f953309bcd4c83e35701fe7a7c4
shkim787 <shkim061198@gmail.com>
2021-07-11 23:10:06 +0900

    feat: pick marker

--- src/components/googleMap/view/GoogleMap.js
+++ src/components/googleMap/view/GoogleMap.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, View,Text, Dimensions } from 'react-native';
+import * as Location from "expo-location";
 
 export default function GoogleMap() {
+   const [isLoading, setIsLoading] = useState(true);
+   const [pos, setPos] = useState();
+
+   const getLocation = async () => {
+      try {
+        await Location.requestForegroundPermissionsAsync();
+        const { coords } = await Location.getCurrentPositionAsync();        
+        setPos(coords)
+        setIsLoading(false);
+      } catch (error) {
+        Alert.alert("Can't find you.", "So sad");
+        history.back();
+      }
+   };
+
+   useEffect(() => {
+      getLocation();      
+   }, []);
    return (
-      <View style={styles.container}>
-        <MapView style={styles.map} /> 
-      </View>
+      <>
+         {isLoading ? (
+            <View style={styles.content}>
+               <Text>Loading...</Text>
+            </View>
+         ):(
+         <View style={styles.container}>
+            <MapView style={styles.map} zoom={20} initialRegion={{ latitude: pos.latitude, longitude: pos.longitude, latitudeDelta: 0.01, longitudeDelta: 0.01 }}> 
+               <MapView.Marker coordinate={{ latitude: pos.latitude, longitude: pos.longitude, }} />
+            </MapView>
+         </View>
+         )}
+      </>
    );
 }
 
@@ -14,6 +43,11 @@ const styles = StyleSheet.create({
    container: {
       flex: 1,
       backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+   },
+   content: {
+      flex: 5,
       alignItems: "center",
       justifyContent: "center",
    },
